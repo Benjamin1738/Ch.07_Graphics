5874435c9e952c46b93380acbeecc5b0f46354c1
sandoval-ordagicben <sandoval-ordagicben@urbandaleschools.com>
2022-11-21 10:13:27 -0600

    ch7

--- 7.0_Jedi_Training.py
+++ 7.0_Jedi_Training.py
@@ -7,3 +7,40 @@ The picture is 500px wide and 400px tall. Look up ARC in the documentation to do
 '''
 import arcade
 
+arcade.open_window(500,400)
+arcade.set_background_color((arcade.color.ALMOND))
+#Background Color
+
+arcade.start_render()
+
+
+#PINK Rectangle
+arcade.draw_rectangle_filled(50,370,60,20,arcade.color.PHLOX)
+
+#Lines Checkers
+for i in range(0,510,20):
+    arcade.draw_line(i,0,i,400,arcade.color.BLACK)
+for i in range(0,410,20):
+    arcade.draw_line(0,i,500,i,arcade.color.BLACK)
+
+#Circle
+arcade.draw_circle_filled(250,200,48,arcade.color.WISTERIA)
+
+#Angled Rectangle
+arcade.draw_rectangle_filled(200,270,40,20,arcade.color.BRICK_RED,-45)
+#Text
+arcade.draw_text("I Love you. I Know",20,165,arcade.color.RED,16,25)
+
+#Diag Line
+arcade.draw_line(80,20,120,60,arcade.color.BLUE)
+
+#Red Dot
+arcade.draw_rectangle_filled(460,10,5,5,arcade.color.RED)
+
+#Pac man
+arcade.draw_arc_filled(400,300,150,150,arcade.color.YELLOW,30,330)
+
+
+arcade.finish_render()
+
+arcade.run()
--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,3 +10,43 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 CHALLENGE: Can you make the entire flag parametrically? This means if I change the hoist to 520px the flag will resize accordingly.
 '''
+import arcade
+arcade.open_window(300,260,"The Stars and Strips")
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+
+#Lines
+for i in range(0,270,20):
+    arcade.draw_line(0,i,310,i,arcade.color.BLACK,1)
+#Blue background
+arcade.draw_rectangle_filled(30,200,190,160,(0,40,104,255))
+
+for r in range(10,260,40):
+    arcade.draw_rectangle_filled(150,r,310,20,arcade.color.RED,0,)
+
+#Blue rec
+arcade.draw_rectangle_filled(30,200,225,160,(0,40,104,255))
+
+#Stars
+
+arcade.draw_text("*  *  *  *  *  *",0,230,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *  *",0,205,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *  *",0,180,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *  *",0,155,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *  *",0,130,arcade.color.WHITE,20,5)
+
+#Second stars
+
+arcade.draw_text("*  *  *  *  *",10,220,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *",10,195,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *",10,165,arcade.color.WHITE,20,5)
+arcade.draw_text("*  *  *  *  *",10,145,arcade.color.WHITE,20,5)
+
+
+
+
+
+
+arcade.finish_render()
+
+arcade.run()
